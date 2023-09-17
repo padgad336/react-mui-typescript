702483aeb8739e2a63898d20f48338deeacbe874
padgad <padgad@gmail.com>
2023-09-17 14:44:42 +0700

    add useeffct

--- src/pages/pokemon-by-id.tsx
+++ src/pages/pokemon-by-id.tsx
@@ -2,11 +2,22 @@ import {
     Typography
 } from '@mui/material'
 import { useParams } from 'react-router-dom'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import pokemomData from './../assets/pokedex.json'
+
 const PokemonById =()=>{
 const params =useParams()
-const [pokemon,setPokemon]=useState()
-// APP .tsx <Route Component={PokemonById} path="/pokemon/:pokemonId" />
+const [pokemon,setPokemon]=useState<any|null>(null)
+const pokemonList =pokemomData
+useEffect(()=>{
+if(!pokemon){
+    let poke =pokemonList.filter((row)=>`${row?.id}`===`${params?.pokemonId}`)
+    if(poke.length !==0){
+
+        setPokemon(poke[0])
+    }
+}
+},[])
 return(
     <>
     <Typography variant='h3'>
